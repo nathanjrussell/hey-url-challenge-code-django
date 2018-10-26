430a9cdf60c386c9819bde7d8487da70c4af26f1
David Mosher <dmosher@fullstacklabs.co>
2018-10-26 14:40:24 -0700

    Removes .DS_Store files, adds .DS_Store to the .gitignore

--- heyurl/models.py
+++ heyurl/models.py
@@ -1,22 +1,11 @@
 from django.db import models
 
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    created_at = models.DateTimeField('date created')
-    updated_at = models.DateTimeField('date updated')
-    def __str__(self):
-        return self.name
-
 class Url(models.Model):
     short_url = models.CharField(max_length=255)
     original_url = models.CharField(max_length=255)
     clicks = models.IntegerField(default=0)
     created_at = models.DateTimeField('date created')
     updated_at = models.DateTimeField('date updated')
-    def __str__(self):
-        return self.original_url
 
 class Click(models.Model):
     url = models.ForeignKey(Url, on_delete=models.CASCADE)
@@ -24,5 +13,3 @@ class Click(models.Model):
     platform = models.CharField(max_length=255)
     created_at = models.DateTimeField('date created')
     updated_at = models.DateTimeField('date updated')
-    def __str__(self):
-        return self.browser
